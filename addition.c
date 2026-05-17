f205218f95e81b35e5892755923f2a0103a31203
Prathmeshpanchlinge2 <panchlingeprathmesh@gmail.com>
2026-05-18 00:38:22 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,16 @@
+/* name:prathGiridhar Panchlinge 
+prn-2503033111372l008
+branch-extc(r)*/
+#include<stdio.h>
+int main()
+{
+	int num1,num2,sum;
+	printf("Enter First Number :");
+	scanf("%d",&num1);
+	printf("Enter Second Number :");
+	scanf("%d",&num2);
+	sum=num1*num2;
+	
+	printf("Your mul is %d*%d=%d",num1,num2,sum);
+	return 0;
+}  
