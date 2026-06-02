5c15c99332f791372a7702b7535631d60a789d89
Prathmeshpanchlinge2 <panchlingeprathmesh@gmail.com>
2026-06-02 09:48:24 +0530

    Add files via upload

---
+++ star.c
@@ -0,0 +1,14 @@
+/* Prathmesh Giridhar Panchlinge 
+prn no.:-2503033111372L008
+star ⭐ */
+
+#include <stdio.h>
+int main(){
+    int i,j,;
+    for(i=1;i<=5;i++){
+        for(k=1;k<=5;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
