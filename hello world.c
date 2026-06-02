2f4636ca99c5ea8d3e59eafe296544e0b255a0ef
Prathmeshpanchlinge2 <panchlingeprathmesh@gmail.com>
2026-06-02 08:51:22 +0530

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,11 @@
+/* prathmesh Giridhar Panchlinge 
+prn n0.- 2503033111372L008
+1st code*/
+
+#include<stdio.h>
+int main(){
+      printf("Hello world");
+      printf("\n prathmesh panchlinge");
+      printf("\n First code");
+      return 0;
+}
