5a7c11585fdddc4f8fed3bd0044b1f80d2611e86
Prathmeshpanchlinge2 <panchlingeprathmesh@gmail.com>
2026-06-02 09:24:21 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,15 @@
+/* Prathmesh Giridhar Panchlinge 
+prn no.2503033111372L008
+Factorial of number */
+
+#include<stdio.h>
+int main (){
+ int i=1,n,fact=1;
+ printf("Enter number: ");
+ scanf("%d",&n);
+ for ( i = 1; i<=n ; i++){
+    fact=fact*i;
+ }
+ printf("Factorial: %d",fact);
+return 0;
+}
